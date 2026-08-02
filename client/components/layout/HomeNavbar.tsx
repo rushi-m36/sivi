1bb1bb2fae0aeb3335ac75f71047798cd9ec16e9
Rushikesh <rushimangore36@gmail.com>
2026-08-02 16:11:07 +0530

    change pos of auth from search-nav to home-nav

--- client/components/layout/HomeNavbar.tsx
+++ client/components/layout/HomeNavbar.tsx
@@ -1,6 +1,13 @@
 "use client";
 
 import { useState } from "react";
+import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 type Tab = "search" | "subscriptions";
 
@@ -50,6 +57,23 @@ export default function HomeNavbar({
             Subscriptions
           </span>
         </button>
+        <div>
+          <ClerkProvider>
+            <header className="flex justify-end items-center p-4 gap-4 h-16">
+              <Show when="signed-out">
+                <SignInButton />
+                <SignUpButton>
+                  <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                    Sign Up
+                  </button>
+                </SignUpButton>
+              </Show>
+              <Show when="signed-in">
+                <UserButton />
+              </Show>
+            </header>
+          </ClerkProvider>
+        </div>
       </div>
     </div>
   );
--- client/components/search/SearchNavbar.tsx
+++ client/components/search/SearchNavbar.tsx
@@ -2,13 +2,6 @@
 
 import { useRouter } from "next/navigation";
 import { SearchBar } from "./SearchBar";
-import {
-  ClerkProvider,
-  Show,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
 
 interface NavbarProps {
   query?: string;
@@ -33,22 +26,6 @@ export function Navbar({ query = "", onSearch }: NavbarProps) {
         <div className="mx-6 flex max-w-xl flex-1">
           <SearchBar onSearch={onSearch} initialValue={query} />
         </div>
-
-        <ClerkProvider>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <Show when="signed-out">
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-purple-700 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </Show>
-            <Show when="signed-in">
-              <UserButton />
-            </Show>
-          </header>
-        </ClerkProvider>
       </div>
     </header>
   );
